4ca1bbdb8a0306f79bc0c588d9265e34f0617cc4
ZefengQiu <z9qiu@uwaterloo.ca>
2020-09-01 21:39:14 -0400

    add file

--- python/routers/router.py
+++ python/routers/router.py
@@ -1,9 +1,16 @@
 from flask import Flask
-
+from service.messageService import MessageService
 
 def init_routes(app):
 
     if app:
 
-        app.add_url_rule('/messages', method=["GET"])
-        app.add_url_rule("/search", methods=["POST"])
+        msgService = MessageService()
+        app.add_url_rule('/messages', 'get_all_messages', msgService.get_all_messages, methods=["GET"])
+
+        """
+        Search for answers!
+        Accepts a 'query' as JSON post, returns the full answer.
+        curl -d '{"query":"Star Trek"}' -H "Content-Type: application/json" -X POST http://localhost:5000/search
+        """
+        # app.add_url_rule("/search", methods=["POST"])
---
+++ python/service/messageService.py
@@ -0,0 +1,15 @@
+from flask import request
+from flask import jsonify
+from flask import make_response
+import json
+import sqlite3
+
+DBPATH = "../database.db"
+
+class MessageService:
+
+    def get_all_messages(self):
+        with sqlite3.connect(DBPATH) as conn:
+            messages_res = conn.execute("select body from messages")
+            messages = [m[0] for m in messages_res]
+            return jsonify(list(messages)), 200
